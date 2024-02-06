00ad61519f85c7afc1bb82f56a9c01e4af757388
wh3248 <wh3248@princeton.edu>
2024-02-06 16:53:54 -0500

    allow data catalog filter by data_catalog_entry_id

--- src/hf_hydrodata/data_catalog.py
+++ src/hf_hydrodata/data_catalog.py
@@ -617,7 +617,7 @@ def _is_row_match_options(row: ModelTableRow, options: dict) -> bool:
     for option in options.keys():
         option_value = options.get(option)
         # Support data catalog filter using data_catalog_entry_id option
-        row_value = row["id"] if option_value == "data_catalog_entry_id" else row[option]
+        row_value = row["id"] if option == "data_catalog_entry_id" else row[option]
         if row_value is not None and option_value and row_value != option_value:
             result = False
             break
