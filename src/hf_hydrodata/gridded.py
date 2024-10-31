8f071370fb5046b0bf871936ba71db1800b34680
wh3248 <wh3248@princeton.edu>
2024-10-31 14:20:29 -0400

    add better comments when not masking

--- src/hf_hydrodata/gridded.py
+++ src/hf_hydrodata/gridded.py
@@ -1251,8 +1251,11 @@ def _apply_mask(data, entry, options):
     The second option masks only against ocean and outer HUC bounds. The first option masks with internal HUC boundaries.
     """
 
-    if options.get("dataset") == "huc_mapping" or options.get("variable") == "clm_run":
-        # Do not mask mask
+    if options.get("dataset") == "huc_mapping":
+        # Do not mask mask the huc_mapping dataset that is used for masking
+        return data        
+    if options.get("variable") == "clm_run":
+        # Do not mask any entry with the clm_run variable that contains vegm data
         return data
     grid = entry.get("grid")
     if grid not in ["conus1", "conus2"]:
