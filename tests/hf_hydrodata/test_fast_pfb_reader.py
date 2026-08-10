5f8e08bf27b05b3e19083a9ca3d087e580a1ab33
hmei-hydro <awshydroframe@gmail.com>
2026-08-10 16:35:22 -0400

    speed up fast_pfb_reader test

--- tests/hf_hydrodata/test_fast_pfb_reader.py
+++ tests/hf_hydrodata/test_fast_pfb_reader.py
@@ -37,20 +37,6 @@ def test_reading_multiple_files():
     assert fast_data.shape == (365, 24, 1, 1)
     assert 2506339.537 == pytest.approx(fast_total, abs=0.001)
 
-def test_not_enough_memory_error():
-    """Test attempting to read a file with so many conus2 sized files that will not fit in memory."""
-
-    if not os.path.exists("/hydrodata"):
-        # Just skip test if this is run on a machine without /hydrodata access
-        return
-
-    with pytest.raises(ValueError):
-        path_template = "/hydrodata/temp/CONUS2_transfers/CONUS2/spinup_WY2003/run_inputs/spinup.wy2003.out.press.*.pfb"
-        pfb_files = glob.glob(path_template)
-        pfb_files.sort()
-        pfb_constraints = None
-        hf_hydrodata.fast_pfb_reader.read_files(pfb_files, pfb_constraints)
-
 
 def test_pqr_too_small():
     """
